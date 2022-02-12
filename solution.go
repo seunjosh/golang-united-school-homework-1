d1f4d2da2bdb3bc37001bd95cdfe40d71cbc30b3
Joshua <seunaji2015@gmail.com>
2022-02-12 14:23:58 +0100

    Update solution.go

--- solution.go
+++ solution.go
@@ -3,15 +3,10 @@ package solution
 import (
          
 	 "github.com/kyokomi/emoji/v2"
-
 )
 
+ func GetMessage() string {
 
-  func GetMessage() string
-  {
-
-	  return emoji.Sprint("Hello :world_map:!") 
-                                               
-          
-  }
+	  return emoji.Sprint("Hello :world_map:!")                                         
+       }
   
